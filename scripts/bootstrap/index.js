7bf1ac69e6d99c24b7ef6d0572c33ef3964ba776
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-10-20 16:53:49 +0200

    🔧 Trying to configure soketi.

--- scripts/bootstrap/index.js
+++ scripts/bootstrap/index.js
@@ -2,11 +2,16 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
-window.Echo = new Echo({
+
+const laravelEcho = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_PUSHER_HOST,
-    wsPort: 443,
-    wssPort: 443,
+    wssHost: process.env.MIX_PUSHER_HOST,
+    wssPort: process.env.MIX_PUSHER_PORT,
+    forceTLS: process.env.MIX_PUSHER_SCHEME === "https",
+    encrypted: true,
     disableStats: true,
+    enabledTransports: ['wss'],
 });
+
+window.Echo = laravelEcho;
