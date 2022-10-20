a868c626a0785f47a2fb6c8508b5b8a7554078a9
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-10-20 18:13:04 +0200

    🐛 Configuring websocket correctly.

--- scripts/bootstrap/index.js
+++ scripts/bootstrap/index.js
@@ -3,15 +3,11 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
-const laravelEcho = new Echo({
+window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_MESSAGING_PUSHER_APP_KEY || "$2y$10$svQIJ8lZEqclI9AqcZEBZeKEqhePKWoFTFU85ML2u9w4aa2wbRHG",
-    wssHost: process.env.MIX_MESSAGING_PUSHER_HOST || "websocket.trustup.io",
+    wsHost: process.env.MIX_MESSAGING_PUSHER_HOST || "websocket.trustup.io",
+    wsPort: process.env.MIX_MESSAGING_PUSHER_PORT || 443,
     wssPort: process.env.MIX_MESSAGING_PUSHER_PORT || 443,
-    forceTLS: (process.env.MIX_MESSAGING_PUSHER_SCHEME || "https") === "https",
-    encrypted: true,
     disableStats: true,
-    enabledTransports: ['wss'],
 });
-
-window.Echo = laravelEcho;
