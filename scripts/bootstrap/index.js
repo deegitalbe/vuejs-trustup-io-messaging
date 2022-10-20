58094c3a5d5d9e91b7a44ce7bd304e7af401c776
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-10-20 17:10:32 +0200

    ✨ Setting default config values for socket.

--- scripts/bootstrap/index.js
+++ scripts/bootstrap/index.js
@@ -5,10 +5,10 @@ window.Pusher = Pusher;
 
 const laravelEcho = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    wssHost: process.env.MIX_PUSHER_HOST,
-    wssPort: process.env.MIX_PUSHER_PORT,
-    forceTLS: process.env.MIX_PUSHER_SCHEME === "https",
+    key: process.env.MIX_MESSAGING_PUSHER_APP_KEY || "$2y$10$svQIJ8lZEqclI9AqcZEBZeKEqhePKWoFTFU85ML2u9w4aa2wbRHG",
+    wssHost: process.env.MIX_MESSAGING_PUSHER_HOST || "websocket.trustup.io",
+    wssPort: process.env.MIX_MESSAGING_PUSHER_PORT || 443,
+    forceTLS: (process.env.MIX_MESSAGING_PUSHER_SCHEME || "https") === "https",
     encrypted: true,
     disableStats: true,
     enabledTransports: ['wss'],
