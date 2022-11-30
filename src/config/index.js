1a7473768e2732599a85eda66a8c7a10745699c6
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-11-30 14:19:22 +0100

    🐛 Processing vite and mix values.

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,9 @@
-const auth_url = MIX_TRUSTUP_AUTH_API_URL || VITE_TRUSTUP_AUTH_API_URL;
-const messaging_url = MIX_MESSAGING_TRUSTUP_API_URL || VITE_MESSAGING_TRUSTUP_API_URL;
-const authorization_header = MIX_TRUSTUP_SERVER_AUTHORIZATION || VITE_TRUSTUP_SERVER_AUTHORIZATION;
+const getViteValue = key => import.meta.env?.[key];
+const getMixValue = key => process.env?.[key];
+
+const auth_url = getMixValue(MIX_TRUSTUP_AUTH_API_URL) || getViteValue(VITE_TRUSTUP_AUTH_API_URL);
+const messaging_url = getMixValue(MIX_MESSAGING_TRUSTUP_API_URL) || getViteValue(VITE_MESSAGING_TRUSTUP_API_URL);
+const authorization_header = getMixValue(MIX_TRUSTUP_SERVER_AUTHORIZATION) || getViteValue(VITE_TRUSTUP_SERVER_AUTHORIZATION);
 
 const config = {
     auth: { url: auth_url, authorization_header },
