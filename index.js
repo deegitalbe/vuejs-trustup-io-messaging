9729a1c8b5d5fefda6f9a90c4af7803986d95a59
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-12-07 20:24:26 +0100

    🐛 Vite is not compatible with require.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import Conversation from "./src/vue/conversation.vue";
+import EXIF from "exif-js";
 
 import Hermite_class from "./src/vendors/hermite"
-window.EXIF = require('exif-js');
+window.EXIF = EXIF;
 window.HERMITE = new Hermite_class()
 
 import Toasteo from "toasteo";
